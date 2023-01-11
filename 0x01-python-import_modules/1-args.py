a2d52d6e2c74a0d08516163c1be18c83b168a8f6
root <5534@holbertonstudents.com>
2023-01-11 02:32:22 +0000

    Updates

--- 0x01-python-import_modules/1-args.py
+++ 0x01-python-import_modules/1-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    numbArm = len(sys.argv) - 1
-    if numbArm == 0:
+    cnt = len(sys.argv) - 1
+    if cnt == 0:
         print("0 arguments.")
-    elif numbArm == 1:
+    elif cnt == 1:
         print("1 argument:")
     else:
-        print("{} arguments:".format(numbArm))
-    for i in range(numbArm):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        print("{} arguments:".format(cnt))
+    for n in range(cnt):
+        print("{}: {}".format(n + 1, sys.argv[n + 1]))
