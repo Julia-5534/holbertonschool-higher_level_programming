9d5450f497afb7eb97cdc0dcf8a68cea660f4098
Julia Bullard <5534@holbertonstudents.com>
2023-01-13 19:28:47 -0600

    Empties

---
+++ 0x02-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+def no_c(my_string):
+    new_string = ""
+    for char_char in my_string:
+        if char_char != "c" and char_char != "C":
+            new_string += char_char
+    return new_string
