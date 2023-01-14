8f19934e203252b265b682f9687beb6ccff9334e
Julia Bullard <5534@holbertonstudents.com>
2023-01-13 21:31:46 -0600

    Fixed Task 0

--- 0x02-python-data_structures/0-print_list_integer.py
+++ 0x02-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-
 def print_integers(lst):
     for item in lst:
         if isinstance(item, int):
             print(item)
 
-# This function takes in a list as an argument and iterates through each item in the list.
-# For each item, it checks if the item is an instance of the int class using the isinstance()
-# function. If it is, it prints the item.
+# This function takes in a list as an argument and iterates through
+# each item in the list. For each item, it checks if the item is an
+# instance of the int class using the isinstance() function. If it is,
+# it prints the item.
