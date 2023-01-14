7790558c4489489b005adf3c203cc0a9dca82499
Julia Bullard <5534@holbertonstudents.com>
2023-01-13 22:07:02 -0600

    Task12

--- 0x02-python-data_structures/11-delete_at.py
+++ 0x02-python-data_structures/11-delete_at.py
@@ -2,6 +2,14 @@
 def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
-    else:
-        new_list = my_list[:idx] + my_list[idx+1:]
-        return new_list
+    del(my_list[idx])
+    return my_list
+
+# It first checks if the provided index is out of range, and if
+# so, it returns the original list without making any changes.
+# If the index is valid, it uses the del statement to remove the
+# item at the specified index and then return the modified list.
+# It is important to note that the function modifies the original
+# list, it will not return a new list with the specified element
+# removed. This is important to consider if you want to keep the
+# original list unchanged.
--- 0x02-python-data_structures/12-switch.py
+++ 0x02-python-data_structures/12-switch.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
+
+# Uses tuple unpacking to unpack the values of a and b into
+# a tuple, and then re-assigns the values of a and b to the
+# unpacked values, swapping them.
