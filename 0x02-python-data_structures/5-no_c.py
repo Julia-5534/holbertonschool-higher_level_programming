5b79aa64b692d474fc1d03fd92a9e2ff90a4602b
Julia Bullard <5534@holbertonstudents.com>
2023-01-13 19:31:38 -0600

    Fix Task 5

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
 def no_c(my_string):
     new_string = ""
     for char_char in my_string:
-        if char_char != "c" and char_char != "C":
+        if char_char != 'c' and char_char != 'C':
             new_string += char_char
     return new_string
