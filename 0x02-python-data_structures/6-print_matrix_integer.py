a74f4fe937cedc0da1841c8516169053a099fc41
Julia Bullard <5534@holbertonstudents.com>
2023-01-13 21:06:03 -0600

    Tasks 6 and 7

--- 0x02-python-data_structures/6-print_matrix_integer.py
+++ 0x02-python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,4 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for item in row:
-            print("{:>5}".format(item), end="")
-        print()
-
-# This function takes in a matrix of integers as an argument and iterates through each row and item in the matrix.
-# For each item, it uses the str.format() method to format the integer as a string with a width of 3 and right-aligns
-# it. The end parameter is set to a space, so that the integers are separated by a space. After each row, it adds a
-# newline character using print() method to separate each row in the matrix.
+        print(' '.join("{:d}".format(val) for val in row))
