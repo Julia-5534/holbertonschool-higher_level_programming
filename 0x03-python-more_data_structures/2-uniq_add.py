a8db7562d49b3337401c0043ffde055eda880d07
Julia Bullard <5534@holbertonstudents.com>
2023-01-17 16:30:55 -0600

    Task 2

--- 0x03-python-more_data_structures/2-uniq_add.py
+++ 0x03-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    # Create a set to store unique integers
+    unique_integers = set()
+    # Iterate through each element in the original list
+    for element in my_list:
+        # If the element is an integer and not already in the set, add to set
+        if isinstance(element, int) and element not in unique_integers:
+            unique_integers.add(element)
+    # Return the sum of the unique integers
+    return sum(unique_integers)
