dd6af5768d812d1509c818d57e490753f186898d
Julia Bullard <5534@holbertonstudents.com>
2023-01-17 16:40:40 -0600

    Task 6

--- 0x03-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x03-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def print_sorted_dictionary(a_dictionary):
+    # Use the built-in function 'sorted' to sort the keys
+    for key in sorted(a_dictionary):
+        print(key, ":", a_dictionary[key])
