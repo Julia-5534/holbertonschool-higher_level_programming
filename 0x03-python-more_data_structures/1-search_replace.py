8fddf4631d1c808632f0f526c5e9a08c1c36c241
Julia Bullard <5534@holbertonstudents.com>
2023-01-17 16:27:07 -0600

    Task 0 & 1

--- 0x03-python-more_data_structures/1-search_replace.py
+++ 0x03-python-more_data_structures/1-search_replace.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+def search_replace(my_list, search, replace):
+    # Create a new list
+    new_list = []
+    # Iterate through each element in the original list
+    for element in my_list:
+        # If element = search element, add replacement element to new list
+        if element == search:
+            new_list.append(replace)
+        # Otherwise, add the original element to the new list
+        else:
+            new_list.append(element)
+    # Return the new list
+    return new_list
