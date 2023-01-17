c2d4613fd6a8a8cc4f27775cc7e16dac0375e5a9
Julia Bullard <5534@holbertonstudents.com>
2023-01-17 17:27:58 -0600

    Task 3

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""The Square Mod Squad"""
+
+
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0):
+        """Initializes a square with a size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Prints area"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Gets a square's size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets a square's size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
