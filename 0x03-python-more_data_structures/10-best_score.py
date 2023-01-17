935c082115c0b542d38cea3c08fa39c748ce3567
Julia Bullard <5534@holbertonstudents.com>
2023-01-17 17:01:43 -0600

    Task 10

--- 0x03-python-more_data_structures/10-best_score.py
+++ 0x03-python-more_data_structures/10-best_score.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    # check if dictionary is empty
+    if not a_dictionary:
+        return None
+    # Use the built-in function 'max' to find the key with the highest value
+    best_key = max(a_dictionary, key=a_dictionary.get)
+    # Return the key with the highest value
+    return best_key
