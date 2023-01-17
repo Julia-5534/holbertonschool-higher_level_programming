11761335d6710b48fff97cfd04c7c23cac2c4b03
Julia Bullard <5534@holbertonstudents.com>
2023-01-17 17:00:04 -0600

    Task 9

--- 0x03-python-more_data_structures/9-multiply_by_2.py
+++ 0x03-python-more_data_structures/9-multiply_by_2.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    # Create a new dictionary
+    new_dictionary = {}
+    # Iterate through each key-value pair in the original dictionary
+    for key, value in a_dictionary.items():
+        # Multiply the value by 2 and add it to the new dictionary
+        new_dictionary[key] = value * 2
+    # Return the new dictionary
+    return new_dictionary
