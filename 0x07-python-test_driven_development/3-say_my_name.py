b8db3e72ac7b8498a9b1d00edb4993370aa61e95
Julia Bullard <5534@holbertonstudents.com>
2023-01-24 22:38:17 -0600

    Task 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,6 +4,8 @@
 
 def say_my_name(first_name, last_name=""):
     """ Function that prints the input names """
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError("first_name and last_name must be strings")
-    print(f"My name is {first_name} {last_name}")
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
