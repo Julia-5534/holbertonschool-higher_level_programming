1ab4f4c7082ebb0bdf6a37c76ffa70190f832076
Julia Bullard <5534@holbertonstudents.com>
2023-01-25 10:09:15 -0600

    Fixed Task 5 Finally

--- 0x02-python-data_structures/5-no_c.py
+++ 0x02-python-data_structures/5-no_c.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    new_string = ""
-    for char_char in my_string:
-        if char_char != 'c' and char_char != 'C':
-            new_string += char_char
-    return new_string
+    return ''.join(char for char in my_string if char not in 'cC')
