cbfb4b5649a10a9632d9057f1577609eae6e7eb7
Julia Bullard <5534@holbertonstudents.com>
2023-01-24 23:30:51 -0600

    Please Get All The Checks

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-""" 5-text_indentation Module """
+"""5-text_indentation Module"""
 
 
 def text_indentation(text):
-    """ Function that adds indentation """
-    if type(text) != str:
+    """Function that prints 2 new lines after '.', '?', & ':'"""
+    if not isinstance(text, str):
         raise TypeError("text must be a string")
-    text = text.replace('. ', ".\n\n")
-    text = text.replace('? ', "?\n\n")
-    text = text.replace(': ', ":\n\n")
+    text = text.replace(".", ".\n\n")
+    text = text.replace("?", "?\n\n")
+    text = text.replace(":", ":\n\n")
+    tokens = text.split("\n")
+    tokens = [token.strip() for token in tokens]
+    text = "\n".join(tokens)
     print(text, end="")
