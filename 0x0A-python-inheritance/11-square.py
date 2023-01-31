f4f50754e69f39057172d0f323acb26996d48529
Julia Bullard <5534@holbertonstudents.com>
2023-01-31 02:35:48 -0600

    Fix Task 11?

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -11,5 +11,9 @@ class Square(Rectangle):
         super().__init__(size, size)
 
 
+def area(self):
+    return self.__size * self.__size
+
+
 def __str__(self):
     return "[Square] {}/{}".format(self.__size, self.__size)
