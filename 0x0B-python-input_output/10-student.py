6e375f17960ddd4b6c6bd52accef7c1e5c08d3ce
Julia Bullard <5534@holbertonstudents.com>
2023-01-31 20:43:35 -0600

    Task 10

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""10-student"""
+
+
+class Student:
+    """Defined by first name, last name, & age"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if type(attrs) == list:
+            if all(type(attr) == str for attr in attrs):
+                return {pp: getattr(self, pp)
+                        for pp in attrs if hasattr(self, pp)}
+        else:
+            return self.__dict__
