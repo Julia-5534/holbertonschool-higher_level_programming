02f256faec099a5f273c6280a0683947e3546d1f
Julia Bullard <5534@holbertonstudents.com>
2023-02-27 16:08:10 -0600

    Add def to Task 1

--- 0x0B-python-object_relational_mapping/1-filter_states.py
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -4,7 +4,11 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-
+    
+    def print_stateN():
+        """
+        Print states from database
+        """
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
 
