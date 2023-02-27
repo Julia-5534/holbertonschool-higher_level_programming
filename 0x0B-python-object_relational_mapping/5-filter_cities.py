4f7dd2d4951ba06caf4c5e6610096041b285df3b
Julia Bullard <5534@holbertonstudents.com>
2023-02-27 16:27:56 -0600

    Pycodestyle fix Task 5

--- 0x0B-python-object_relational_mapping/5-filter_cities.py
+++ 0x0B-python-object_relational_mapping/5-filter_cities.py
@@ -6,7 +6,9 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name=%s ORDER BY cities.id ASC", (sys.argv[4],))
+    cur.execute("SELECT cities.name FROM cities JOIN states ON\
+                cities.state_id = states.id WHERE states.name=%s\
+                ORDER BY cities.id ASC;", (sys.argv[4],))
     rows = cur.fetchall()
     print(", ".join(city[0] for city in rows))
     cur.close()
