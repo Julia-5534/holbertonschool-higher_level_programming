f6abe26cece1ba86b94d6266abaca6a481896b5f
Julia-5534 <112386137+Julia-5534@users.noreply.github.com>
2023-02-28 16:43:03 -0600

    Update 9-model_state_filter_a.py

--- 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0B-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""
-lists all State objects that contain the letter a from the database hbtn_0e_6_usa
-"""
+""" Task 9 """
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,17 +8,21 @@ from model_state import Base, State
 
 if __name__ == "__main__":
     # Create a new Engine instance.
-    # The Engine is the starting point for any SQLAlchemy application.
-    # It’s “home base” for the actual database and its DBAPI, delivered
-    # to the SQLAlchemy application through a connection pool and a Dialect.
+    # The Engine is the starting point for any
+    # SQLAlchemy application.
+    # It’s “home base” for the actual database and
+    # its DBAPI, delivered to the SQLAlchemy application
+    # through a connection pool and a Dialect.
     # The connection pool provides a source of distinct Connection objects
     # that are owned by the application, for use with the database.
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
-    # This is the function that allows you to interact with the database
-    # In order to do so, you need a session, which is an abstraction
-    # layer over the transactional system
+    # This is the function that allows you to
+    # interact with the database
+    # In order to do so, you need a session,
+    # which is an abstraction layer over the
+    # transactional system
     Session = sessionmaker(bind=engine)
     session = Session()
 
