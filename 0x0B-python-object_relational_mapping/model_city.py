e5b8eade9df6600aa2c3a9f28ea4c98e4950a3df
Julia-5534 <112386137+Julia-5534@users.noreply.github.com>
2023-02-28 16:48:10 -0600

    Update model_city.py

--- 0x0B-python-object_relational_mapping/model_city.py
+++ 0x0B-python-object_relational_mapping/model_city.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-"""
-This module defines the City class.
-"""
-
-from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base
+"""City Class"""
+from sqlalchemy import String, Integer, Column, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 
 class City(Base):
-    """Represents a city for a given state."""
+    """subclass of Base"""
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
-    def __repr__(self):
-        return f"City(id={self.id}, name='{self.name}', state_id={self.state_id})"
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
