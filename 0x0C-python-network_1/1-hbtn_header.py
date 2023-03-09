e71b2cc01df1437e3f594906514145e0b4338c21
Julia Bullard <5534@holbertonstudents.com>
2023-03-09 02:51:55 -0600

    Final?

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -4,14 +4,11 @@ Script that takes in a URL, sends a request to the
 URL and displays the value of the `X-Request-Id`
 variable found in the header of the response.
 """
-import urllib.request
+from urllib import request
 import sys
 
 if __name__ == "__main__":
     http = sys.argv[1]
-    req = urllib.request.Request(http)
-    with urllib.request.urlopen(req) as response:
-        headers = response.getheaders()
-        x_request_id = [x[1] for x in headers if x[0] == 'X-Request-Id'][0]
-
-print(x_request_id)
+    req = request.Request(http)
+    with request.urlopen(req) as response:
+        print(response.info()['X_Request_Id'])
