13d8534265e7f216b9fb56c428264220c2ce5184
Julia-5534 <112386137+Julia-5534@users.noreply.github.com>
2023-03-09 02:07:05 -0600

    Execute

--- 0x0C-python-network_1/0-hbtn_status.py
+++ 0x0C-python-network_1/0-hbtn_status.py
@@ -7,6 +7,7 @@ url = 'https://intranet.hbtn.io/status'
 with urllib.request.urlopen(url) as response:
     html = response.read()
 
+#Commenting to save change to execute
 print("Body response:")
 print("\t- type: {}".format(type(html)))
 print("\t- content: {}".format(html))
