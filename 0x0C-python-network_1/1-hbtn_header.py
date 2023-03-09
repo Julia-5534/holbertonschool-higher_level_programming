9d4ec5b918d1b32d39e466d908a72f1e36022926
Julia Bullard <5534@holbertonstudents.com>
2023-03-09 02:32:17 -0600

    Fixed Task 1

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -2,12 +2,10 @@
 import urllib.request
 import sys
 
-# get the URL from the command line argument
 url = sys.argv[1]
 
-# send a request to the URL and get the response
 with urllib.request.urlopen(url) as response:
-    # get the value of the X-Request-Id variable from header of response
-    x_request_id = response.getheader('X-Request-Id')
-    # display the value of the X-Request-Id variable
-    print(x_request_id)
+    header_dict = dict(response.getheaders())
+    x_request_id = header_dict.get('X-Request-Id')
+
+print(x_request_id)
