fab698e96172a2cd0e61e74bffb68b222d723c8d
Julia Bullard <5534@holbertonstudents.com>
2023-03-09 03:56:30 -0600

    Task 10

--- 0x0C-python-network_1/10-my_github.py
+++ 0x0C-python-network_1/10-my_github.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     username = sys.argv[1]
     token = sys.argv[2]
     url = "https://api.github.com/user"
-    requests = get(url, auth=HTTPBasicAuth(username, token).json())
-    site = requests.json()["id"]
+    requests = get(url, auth=HTTPBasicAuth(username, token)).json()
+    site = requests.get('id')
     print(site)
