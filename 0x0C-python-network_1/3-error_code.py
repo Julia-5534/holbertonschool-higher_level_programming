695ccf8411f78a65f3cb46e93bf23c6ab4166814
Julia Bullard <5534@holbertonstudents.com>
2023-03-09 03:13:04 -0600

    Task 3

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -3,14 +3,14 @@
 Script that takes in a URL, sends a request to the URL and displays
 the body of the response.
 """
-from urllib.request import urlopen
+from urllib.request import Request, urlopen
 from urllib.error import HTTPError
 from sys import argv
 
 if __name__ == "__main__":
-    url = argv[1]
+    url = Request(argv[1])
     try:
-        with urlopen(url) as response:
-            print(response.read().decode("utf-8"))
-    except HTTPError as e:
-        print(f"Error code: {e.code}")
+        with urlopen(url) as site:
+            print(site.read().decode("utf-8"))
+    except HTTPError as error:
+        print(f"Error code: {error.code}")
