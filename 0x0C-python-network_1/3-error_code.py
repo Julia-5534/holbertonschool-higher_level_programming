cc0b3ff7a3f50d3d360d90d7d999b0c34d8bc4ce
Julia Bullard <5534@holbertonstudents.com>
2023-03-09 03:09:38 -0600

    Fixed module documentation

--- 0x0C-python-network_1/3-error_code.py
+++ 0x0C-python-network_1/3-error_code.py
@@ -3,7 +3,7 @@
 Script that takes in a URL, sends a request to the URL and displays
 the body of the response.
 """
-from urllib.request import Request, urlopen
+from urllib.request import urlopen
 from urllib.error import HTTPError
 from sys import argv
 
@@ -11,7 +11,6 @@ if __name__ == "__main__":
     url = argv[1]
     try:
         with urlopen(url) as response:
-            body = response.read().decode("utf-8")
-            print(body)
+            print(response.read().decode("utf-8"))
     except HTTPError as e:
         print(f"Error code: {e.code}")
