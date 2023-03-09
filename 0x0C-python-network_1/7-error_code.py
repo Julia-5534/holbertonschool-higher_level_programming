aa300df54b2f5759781a739672a5f05da9cfe43d
Julia Bullard <5534@holbertonstudents.com>
2023-03-09 03:35:32 -0600

    Task 7 mod documentation

--- 0x0C-python-network_1/7-error_code.py
+++ 0x0C-python-network_1/7-error_code.py
@@ -4,13 +4,13 @@ Script that takes in a URL, sends a request to the URL and displays
 the body of the response. If the HTTP status code is greater than or equal
 to 400, prints: Error code: followed by the value of the HTTP status code
 """
-import requests
+from requests import get
 from sys import argv
 
 if __name__ == "__main__":
     url = argv[1]
-    site = requests.get(url)
+    site = get(url)
     if site.status_code >= 400:
-        print(f"Error code: {site.status_code}")
+        print("Error code: {}".format(site.status_code))
     else:
         print(site.text)
