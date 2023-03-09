277aa62139cd3742e37753cbd77ec4ce501b9b85
Julia Bullard <5534@holbertonstudents.com>
2023-03-09 02:46:56 -0600

    http

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -8,8 +8,8 @@ import urllib.request
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    req = urllib.request.Request(url)
+    http = sys.argv[1]
+    req = urllib.request.Request(http)
     with urllib.request.urlopen(req) as response:
         headers = response.getheaders()
         x_request_id = [x[1] for x in headers if x[0] == 'X-Request-Id'][0]
