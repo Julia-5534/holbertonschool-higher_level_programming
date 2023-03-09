d5d8801afdeec9de418f459454c944ab093a46d2
Julia Bullard <5534@holbertonstudents.com>
2023-03-09 02:45:06 -0600

    added if statement

--- 0x0C-python-network_1/1-hbtn_header.py
+++ 0x0C-python-network_1/1-hbtn_header.py
@@ -7,10 +7,11 @@ variable found in the header of the response.
 import urllib.request
 import sys
 
-url = sys.argv[1]
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    headers = response.getheaders()
-    x_request_id = [x[1] for x in headers if x[0] == 'X-Request-Id'][0]
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        headers = response.getheaders()
+        x_request_id = [x[1] for x in headers if x[0] == 'X-Request-Id'][0]
 
 print(x_request_id)
